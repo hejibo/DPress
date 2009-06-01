eb1dce8c93968fc0e79117aabf5fff9c63e88a03
vicalloy <vicalloy@4b9082fe-49e7-11de-8001-7f2440edb24e>
2009-06-01 06:52:30 +0000

    - debug info

--- site/dpress/dpressblog/templatetags/pagination_tags.py
+++ site/dpress/dpressblog/templatetags/pagination_tags.py
@@ -11,6 +11,5 @@ def render_paginate(context):
             del getvars['page']
         if len(getvars.keys()) > 0:
             context.update({"getvars": "&%s" % getvars.urlencode()})
-        print "==========", context
         return context
 register.inclusion_tag('pagination/pagination.html', takes_context=True)(render_paginate)
